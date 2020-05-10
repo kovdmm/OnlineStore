8ef7a0cc94aadb214f34dc58183fcd76fcfba2a6
kovdmm <kov.dmm@gmail.com>
2020-05-10 16:37:27 +0300

    Update "Remove from Cart" functionality
    1. Small refactoring in Apex Controller
    2. Add functions in Controller.js and Helper.js

--- force-app/main/default/aura/onlineStoreCart/onlineStoreCartController.js
+++ force-app/main/default/aura/onlineStoreCart/onlineStoreCartController.js
@@ -2,5 +2,13 @@
     init: function(component, event, helper) {
         var userId = component.get('v.user').Id;
         helper.retrieveCart(component, userId);
+    },
+    clickRemoveFromCart: function(component, event, helper) {
+        var userId = component.get('v.user').Id;
+        var productId = event.getSource().get('v.name');
+
+        console.log('[onlineStoreCart.clickRemoveFromCart] userId =', userId, 'productId =', productId);
+
+        helper.removeFromCart(component, userId, productId);
     }
 });
--- force-app/main/default/aura/onlineStoreCart/onlineStoreCartHelper.js
+++ force-app/main/default/aura/onlineStoreCart/onlineStoreCartHelper.js
@@ -18,6 +18,32 @@
         });
     },
 
+    removeFromCart: function(component, userId, productId) {
+        var action = component.get('c.removeFromCart');
+        action.setParams({
+            'userId': userId,
+            'productId': productId
+        });
+        this.fireRemoteCall(action, function (response) {
+            var state = response.getState();
+            if (state === 'SUCCESS') {
+                var deleted = response.getReturnValue();
+
+                console.log('[REMOTE CALL] [OnlineStoreProductsController.removeFromCart] deleted =', deleted);
+
+                var cart = component.get('v.cart');
+                
+                var cart = cart.filter(function(cartItem) {
+                    return cartItem.ProductId__c != productId;
+                });
+
+                component.set('v.cart', cart);
+            } else {
+                console.log('[REMOTE CALL] [OnlineStoreProductsController.removeFromCart] state =', state);
+            }
+        });
+    },
+
     fireRemoteCall: function(action, callback) {
         action.setCallback(this, callback);
         $A.enqueueAction(action);
