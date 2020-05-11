7a82388ab0a24210fe05661e5219b8cae5e22174
kovdmm <kov.dmm@gmail.com>
2020-05-11 04:54:35 +0300

    Refactor cart functionality

--- force-app/main/default/aura/onlineStoreCart/onlineStoreCartHelper.js
+++ force-app/main/default/aura/onlineStoreCart/onlineStoreCartHelper.js
@@ -22,6 +22,25 @@
     retrievePaidItems: function(component, userId) {
         var action = component.get('c.getPaidItems');
         action.setParams({
+            'userId': userId
+        });
+        this.fireRemoteCall(action, function (response) {
+            var state = response.getState();
+            if (state === 'SUCCESS') {
+                var paidItems = response.getReturnValue();
+
+                console.log('[REMOTE CALL] [OnlineStoreProductsController.getPaidItems] paidItems =', JSON.stringify(paidItems));
+
+                component.set('v.paidItems', paidItems);
+            } else {
+                console.log('[REMOTE CALL] [OnlineStoreProductsController.getPaidItems] state =', state);
+            }
+        });
+    },
+
+    removeFromCart: function(component, userId, productId) {
+        var action = component.get('c.removeFromCart');
+        action.setParams({
             'userId': userId,
             'productId': productId
         });
@@ -37,28 +56,9 @@
                 });
 
                 component.set('v.cart', cart);
-                component.set('v.totalPrice', this.calculateBill(cart));
+                component.set('v.cartTotalPrice', this.calculateBill(cart));
             } else {
                 console.log('[REMOTE CALL] [OnlineStoreProductsController.removeFromCart] state =', state);
-            }
-        });
-    },
-
-    createOrder: function(component, userId) {
-        var action = component.get('c.createOrder');
-        action.setParams({
-            'userId': userId
-        });
-        this.fireRemoteCall(action, function (response) {
-            var state = response.getState();
-            if (state === 'SUCCESS') {
-                var order = response.getReturnValue();
-
-                console.log('[REMOTE CALL] [OnlineStoreProductsController.createOrder] order =', JSON.stringify(order));
-
-                // Go to updaid orders page to perform payment for order
-            } else {
-                console.log('[REMOTE CALL] [OnlineStoreProductsController.createOrder] state =', state);
             }
         });
     },
@@ -70,6 +70,16 @@
             bill += cartItem.Quantity__c * cartItem.ProductId__r.Price__c;
         }
         return bill.toFixed(2);
+    },
+
+    payForOrder: function(component, userId) {
+        this.redirect('paid');
+    },
+
+    redirect: function(location) {
+        var event = $A.get('e.c:locationUpdateEvent');
+        event.setParam('location', location);
+        event.fire();
     },
 
     fireRemoteCall: function(action, callback) {
